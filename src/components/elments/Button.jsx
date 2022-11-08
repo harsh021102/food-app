0c2491a9fa3cf0c0d83f615dde91e1a1a29adf34
harsh021102 <harsh021102@gmail.com>
2022-11-09 00:06:48 +0530

    38:19

--- src/components/elments/Button.jsx
+++ src/components/elments/Button.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-
-const Button = ({children}) => {
+import clsx from "clsx"
+function getClassName({className}){
+    return clsx(
+        'text-white text-lg font-bold rounded-xl transition duration-300 cursor-pointer focus:outline-none focus:ring-opacity-50',
+        className
+    )
+}
+const sizes = {
+    small: 'px-4 py-3',
+    medium: 'px-6 py-4',
+    large: 'w-full px-4 py-3'
+}
+const variants = {
+    primary: 'big-marigold focus:ring-marigold',
+    secondry: 'bg-tomato focus:ring-tomato',
+    large: 'bg-black focus:ring-white',
+};
+const Button = ({
+    children,
+    className,
+    size="small",
+    variant='primary',
+...rest}) => {
   return (
-    <button>
+    <button className={clsx(
+    sizes[size],
+    variants[variant],
+    getClassName({className}))}>
         {children}
     </button>
   )
